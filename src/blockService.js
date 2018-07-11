7eedcc73583ce94f715e1523c5c59cf7e1ecd3b1
Christian Stano <christian.p.stano@vanderbilt.edu>
2018-07-11 13:23:08 -0400

    Cleaned Code

--- src/blockService.js
+++ src/blockService.js
@@ -55,13 +55,6 @@ class BlockService {
     overlapPrevention(maxArr,distance) {
       var max = Math.max(...maxArr); //find max depth possible
       this.visibleBlocks.forEach(b => {
-        //determine if block is a destination block, is the last block of t
-        // if ((this.getBlock(b.key).utilities).length > 2) {
-        //   var offset = 0;
-        //   this.getBlock(b.key).utilities.forEach(u => {
-        //     this.getBlock(u).loc = ((this.getBlock(u).x+offset)*distance).toString() + " " + (this.getBlock(u).y*distance).toString();
-        //   })
-        // }
         if (this.isDestination(b.key) && this.getBlock(b.key).destination === "" && this.hasUtilities(b.key)) {
           if (this.getBlock(b.key).y < max) {
             this.getBlock(b.key).loc = (this.getBlock(b.key).x).toString() + " " + ((max+1)*distance).toString();
